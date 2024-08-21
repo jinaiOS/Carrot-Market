905eb00c24fc6de59c2a9a15c1acfcdf3de2fa33
jinaiOS <kj227777@naver.com>
2024-08-21 20:56:28 +0900

    [FEAT]: 서버에서 내려주는 데이터 화면에 보여줌, 최신 게시글 순으로, 시간 계산해서 보여줌

--- static/js/index.js
+++ static/js/index.js
@@ -2,6 +2,19 @@ document.getElementById("goToChat").addEventListener("click", function () {
   window.location.href = "http://127.0.0.1:8000/chat.html";
 });
 
+const calcTime = (timestamp) => {
+  const curTime = new Date().getTime() - 9 * 60 * 60 * 1000;
+  const time = new Date(curTime - timestamp);
+  const hour = time.getHours();
+  const minute = time.getMinutes();
+  const second = time.getSeconds();
+
+  if (hour > 0) return `${hour}시간 전`;
+  else if (minute > 0) return `${minute}분 전`;
+  else if (second > 0) return `${second}초 전`;
+  else "방금 전";
+};
+
 const renderData = (data) => {
   const main = document.querySelector("main");
 
@@ -11,9 +24,39 @@ const renderData = (data) => {
     return;
   }
 
-  data.forEach((obj) => {
+  data.reverse().forEach((obj) => {
     const div = document.createElement("div");
-    div.innerText = obj.title; // obj.title이 존재하는지 확인 필요
+    div.className = "item-list";
+
+    const imgDiv = document.createElement("div");
+    imgDiv.className = "item-list__img";
+
+    const img = document.createElement("img");
+    img.src = "assets/img.svg";
+
+    const infoDiv = document.createElement("div");
+    infoDiv.className = "item-list__info";
+
+    const infoTitleDiv = document.createElement("div");
+    infoTitleDiv.className = "item-list__info-title";
+    infoTitleDiv.innerText = obj.title;
+
+    const infoMetaDiv = document.createElement("div");
+    infoMetaDiv.className = "item-list__info-meta";
+    infoMetaDiv.innerText = obj.place + " " + calcTime(obj.insertAt);
+
+    const infoPriceDiv = document.createElement("div");
+    infoPriceDiv.className = "item-list__info-price";
+    infoPriceDiv.innerText = obj.price;
+
+    imgDiv.appendChild(img);
+    infoDiv.appendChild(infoTitleDiv);
+    infoDiv.appendChild(infoMetaDiv);
+    infoDiv.appendChild(infoPriceDiv);
+
+    div.appendChild(imgDiv);
+    div.appendChild(infoDiv);
+
     main.appendChild(div);
   });
 };
